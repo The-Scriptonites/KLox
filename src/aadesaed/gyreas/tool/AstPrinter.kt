6cc4efb320e596b2415a5b268454e801e745231a
Saheed Adeleye <aadesaed@gmail.com>
2024-07-03 08:58:25 +0100

    some pretty-printing escapes

--- src/aadesaed/gyreas/tool/AstPrinter.kt
+++ src/aadesaed/gyreas/tool/AstPrinter.kt
@@ -6,6 +6,9 @@ import TokenType.STAR
 import aadesaed.gyreas.klox.*
 
 class AstPrinter : Visitor<String> {
+    private val INDENT_WIDTH = 2
+    private var indentLevel = 0
+
     fun print(expr: Expr): String {
         return expr.accept(this)
     }
@@ -31,30 +34,34 @@ class AstPrinter : Visitor<String> {
         val printer = this
 
         return buildString {
+
+            if (indentLevel > 0) append("\n")
+            append(" ".repeat(indentLevel))
             append("($name")
+
+            indentLevel += INDENT_WIDTH
+
             exprs.forEach { expression ->
-                append(" ${expression.accept(printer)}")
+                append(" ")
+                append(expression.accept(printer))
             }
             append(")")
+            if (indentLevel == 0) append("\n")
+
+            indentLevel -= INDENT_WIDTH
         }
     }
 }
 
-fun main(args: Array<String>) {
+fun main() {
     val expression = Binary(
         Grouping(
             Binary(
-                Literal(1),
-                Token(MINUS, "+", null, 1),
-                Literal(2)
+                Literal(1), Token(MINUS, "+", null, 1), Literal(2)
             )
-        ),
-        Token(STAR, "*", null, 1),
-        Grouping(
+        ), Token(STAR, "*", null, 1), Grouping(
             Binary(
-                Literal(4),
-                Token(MINUS, "-", null, 1),
-                Literal(3)
+                Literal(4), Token(MINUS, "-", null, 1), Literal(3)
             )
         )
     )
